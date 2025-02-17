370694905bd785a4504bdf2d7004ed76d39a7935
VivekKrPathak <Vkp10x27@gmail.com>
2025-02-17 20:13:21 +0530

    Build Feed API & Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { userauth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
+const USER_SAFE_DATA = "firstName lastName"
 
 // Get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userauth, async (req, res) => {
@@ -11,7 +13,7 @@ userRouter.get("/user/requests/received", userauth, async (req, res) => {
         const connectionRequests = await ConnectionRequestModel.find({
             toUserId: loggedInUser._id,
             status: "interested",
-        }).populate("fromUserId", "firstName lastName"); //  ["firstName", "lastName"] / "firstName lastName" this will return only two parameters but if dont give anything then it will return whole data from DB and that's a bad thing and it breaches the user data.
+        }).populate("fromUserId", USER_SAFE_DATA); //  ["firstName", "lastName"] / "firstName lastName" this will return only two parameters but if dont give anything then it will return whole data from DB and that's a bad thing and it breaches the user data.
         // Ref and populate are same as Join in MySQL
 
         res.json({
@@ -33,10 +35,8 @@ userRouter.get("/user/connections", userauth, async (req, res) => {
                 { fromUserId: loggedInUser._id, status: "accepted" }
             ]
         })
-            .populate("fromUserId", "firstName lastName")
-            .populate("toUserId", "firstName lastName");
-
-            console.log(connectionAccepted);
+            .populate("fromUserId", USER_SAFE_DATA)
+            .populate("toUserId", USER_SAFE_DATA);
 
         const data = connectionAccepted.map((row) => {
             if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
@@ -52,6 +52,45 @@ userRouter.get("/user/connections", userauth, async (req, res) => {
     } catch (err) {
         res.status(400).json({ message: "ERROR : " + err.message });
     }
-})
+});
+
+userRouter.get("/user/feed", userauth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const page = parseInt(req.query.page) || 1;
+        const skip = (page - 1) * limit;
+
+        // Find all the connection requests(send + received)
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or: [
+                { toUserId: loggedInUser._id },
+                { fromUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        })
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },  // Array.from() this is use for converting set, objects into Array.
+                { _id: { $ne: loggedInUser._id } },
+            ]
+        })
+            .select(USER_SAFE_DATA + " about skills")
+            .skip(skip)
+            .limit(limit);
+
+        res.json({users});
+    } catch (err) {
+        res.status(400).json({ message: "ERROR : " + err.message });
+    }
+});
 
 module.exports = userRouter;
